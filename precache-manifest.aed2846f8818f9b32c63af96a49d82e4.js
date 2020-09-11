195bb9cca2b8f914e27555f8142601cdf1243382
rwabusini <rwabusini@gmail.com>
2020-09-11 18:09:12 +0300

    Updates

---
+++ precache-manifest.aed2846f8818f9b32c63af96a49d82e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d813a2bed1d375672271e43ce82e26",
+    "url": "/Video/index.html"
+  },
+  {
+    "revision": "fcbad9f0c4e0f11efec1",
+    "url": "/Video/static/css/main.f43882dc.chunk.css"
+  },
+  {
+    "revision": "9250bd5cac582670079c",
+    "url": "/Video/static/js/2.fbe52040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Video/static/js/2.fbe52040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbad9f0c4e0f11efec1",
+    "url": "/Video/static/js/main.c9705720.chunk.js"
+  },
+  {
+    "revision": "6d55c8fbdfb7b49e6a98",
+    "url": "/Video/static/js/runtime-main.fa65a298.js"
+  }
+]);
