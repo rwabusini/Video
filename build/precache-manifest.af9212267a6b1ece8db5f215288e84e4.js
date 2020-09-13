1d1213666de8096421aa1cd5fe01b24b5eb08759
rwabusini <rwabusini@gmail.com>
2020-09-13 12:01:51 +0300

    new deploy

--- build/precache-manifest.af9212267a6b1ece8db5f215288e84e4.js
+++ build/precache-manifest.af9212267a6b1ece8db5f215288e84e4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "27d813a2bed1d375672271e43ce82e26",
+    "revision": "c7026ece4ed65788bf66b7e1fd7d7154",
     "url": "/Video/index.html"
   },
   {
-    "revision": "fcbad9f0c4e0f11efec1",
+    "revision": "2c712e49e2a285575a46",
     "url": "/Video/static/css/main.f43882dc.chunk.css"
   },
   {
-    "revision": "9250bd5cac582670079c",
-    "url": "/Video/static/js/2.fbe52040.chunk.js"
+    "revision": "4a76a66d8a68b7a05152",
+    "url": "/Video/static/js/2.46654a16.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/Video/static/js/2.fbe52040.chunk.js.LICENSE.txt"
+    "url": "/Video/static/js/2.46654a16.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fcbad9f0c4e0f11efec1",
-    "url": "/Video/static/js/main.c9705720.chunk.js"
+    "revision": "2c712e49e2a285575a46",
+    "url": "/Video/static/js/main.244cadcc.chunk.js"
   },
   {
     "revision": "6d55c8fbdfb7b49e6a98",
